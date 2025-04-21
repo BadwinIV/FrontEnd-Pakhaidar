70ffc6d97569510035f266da04c23049306f41fd
adammaulidan <adamteyenk@gmail.com>
2025-04-21 14:25:11 +0700

    Praktikum Pertemuan 9

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Greeting from './components/Greeting';
+import ProductList from './components/productList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Belajar React Frontend</h1>
+        <Greeting name="Pinarak Para Mahasiswa Sedoyo" />
+        <ProductList />
+    </div>
+ );
+}
+export default App;
---
+++ src/components/greeting.jsx
@@ -0,0 +1,4 @@
+function Greeting(props) {
+    return <h2>Monggo, {props.name}!</h2>;
+   }
+   export default Greeting;
