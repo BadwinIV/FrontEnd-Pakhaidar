f93ec176b01738f67544b4169feafbee54b5689f
adammaulidan <adamteyenk@gmail.com>
2025-04-21 14:47:38 +0700

    Menambahkan mahcomponent mahasiswa & postlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Greeting from './components/Greeting';
 import ProductList from './components/productList';
+import Mahasiswa from './components/mahasiswa';
+import PostList from './components/PostList';
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <h1>Belajar React Frontend</h1>
         <Greeting name="Pinarak Para Mahasiswa Sedoyo" />
         <ProductList />
+        <Mahasiswa nama="AdamMaulidan" nrp="3124522013" prodi="TeknikInformatika" />
+        <PostList />
     </div>
  );
 }
